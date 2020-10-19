3092a54e909b359cee495f448672c271e1ff6833
Maryam Chaudhry <maryamc1798@gmail.com>
2020-10-18 21:59:18 -0400

    added js file and surprisingly appended current date to jumbotron

---
+++ script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    // using moment.js to display the current date
+    var currentDate = moment().format('MMMM Do YYYY, h:mm:ss a');
+    $("#currentDay").append(currentDate);
+    console.log(currentDate)
+
+    //setting variables for times of day
+
+
+
+
+
+
+
+
+
+
+
+
+})
