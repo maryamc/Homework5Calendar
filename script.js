8440685821de526f54d71cd649c43402541bde10
Maryam Chaudhry <maryamc1798@gmail.com>
2020-10-18 23:09:51 -0400

    trying to save to local storage

--- script.js
+++ script.js
@@ -2,9 +2,25 @@ $(document).ready(function(){
     // using moment.js to display the current date
     var currentDate = moment().format('MMMM Do YYYY, h:mm:ss a');
     $("#currentDay").append(currentDate);
-    console.log(currentDate)
+    console.log(currentDate);
 
-    //setting variables for times of day
+    
+    
+
+
+
+
+
+
+
+
+    //setting on click event for save button 
+    $(".saveBtn").on("click", function (){
+        var event = $(this).siblings(".form-control").val();
+        var time = $(this).parent().attr("id");
+        localStorage.setItem(event, time); 
+
+    })
 
 
 
